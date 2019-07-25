1d22cecda48921adcb5f45bde0bc2b92e2df9397
rahiminasab2015 <rahiminasab.e@husky.neu.edu>
2019-07-25 19:57:42 -0400

    added the translate funtion

--- persian.py
+++ persian.py
@@ -4,7 +4,12 @@ def readfile(path):
     with open(path) as f:
         return f.readlines()
 
+def translate(lines):
+    #TODO: implement the translation
+    return lines
+
 if __name__ == "__main__":
     file_path = sys.argv[1]
     lines = readfile(file_path)
+    translated_lines = translate(lines)
     print(lines)
